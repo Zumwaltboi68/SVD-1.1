28ad2f08cc9cf4b842391b4edb706cb50715b474
Manjushri Bodhisattva <Manjushri@users.noreply.huggingface.co>
2022-12-02 20:01:16 +0000

    Update app.py
    Increase Guidance Scale makes CPU usage exponentially slower...

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ model_id = "stabilityai/stable-diffusion-x4-upscaler"
 pipeline = StableDiffusionUpscalePipeline.from_pretrained(model_id)
 pipeline = pipeline.to("cpu")
 #define interface 
-def upscale(low_res_img, prompt, guide):
+def upscale(low_res_img, prompt):
  low_res_img = Image.open(low_res_img).convert("RGB")
  low_res_img = low_res_img.resize((128, 128))
- upscaled_image = pipeline(prompt=prompt, image=low_res_img, guidance_scale=guide).images[0]
+ upscaled_image = pipeline(prompt=prompt, image=low_res_img, guidance_scale=1).images[0]
  upscaled_image.save("upsampled.png")
  return upscaled_image
 #launch interface
-gr.Interface(fn=upscale, inputs=[gr.Image(type='filepath'), 'text', gr.Slider(1, 20, 5)], outputs=gr.Image(type='filepath')).launch(max_threads=True, debug=True)
+gr.Interface(fn=upscale, inputs=[gr.Image(type='filepath'), 'text'], outputs=gr.Image(type='filepath')).launch(max_threads=True, debug=True)
