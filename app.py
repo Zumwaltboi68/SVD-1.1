64a40e6d0316ebd3bb60c4357021f66b9b07ff60
Manjushri Bodhisattva <Manjushri@users.noreply.huggingface.co>
2023-04-05 19:13:18 +0000

    Update app.py
    Increased number of iterations

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ gr.Interface(fn=upscale, inputs=[
     gr.Textbox(label="Optional: Enter a Prompt to Slightly Guide the AI's Enhancement"), 
     gr.Textbox(label='Experimental: Slightly influence What you do not want the AI to Enhance.'), 
     gr.Slider(2, 15, 7, step=1, label='Guidance Scale: How much the AI influences the Upscaling.'), 
-    gr.Slider(5, 25, 10, step=1, label='Number of Iterations')], 
+    gr.Slider(5, 50, 25, step=1, label='Number of Iterations')], 
     outputs=gr.Image(type="filepath", label = 'Upscaled Image'), 
     title='SD Upscaler', 
     description='2x Latent Upscaler using SD 2.0 And 4x Upscaler using SD 2.1. This version runs on CPU or GPU and is currently running on a T4 GPU. For 4x Upscaling use images lower than 512x512. For 2x Upscaling use 512x512 to 768x768 images.<br><br><b>Notice: Largest Accepted Resolution is 768x768', 
