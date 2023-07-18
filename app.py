578a3a4a397b9443925a8c579f93351c9f48ae18
Manjushri Bodhisattva <Manjushri@users.noreply.huggingface.co>
2023-07-18 01:01:25 +0000

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def refiner (prompt, negative_prompt, scale, steps, seed):
     int_image = genie(prompt, negative_prompt, scale, steps, seed)
     int_image
     #int_image.save('source.png')
-    image = refiner(prompt=prompt, image=int_image).images[0]
+    image = refiner(prompt=prompt, int_image=int_image).images[0]
     return image
 
     
