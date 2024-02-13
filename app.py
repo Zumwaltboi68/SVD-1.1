f2ba17b82410f87ef30f4f725fe5715f08d4211c
Manjushri Bodhisattva <Manjushri@users.noreply.huggingface.co>
2024-02-13 02:45:29 +0000

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def sample(
     randomize_seed: bool = True,
     motion_bucket_id: int = 127,
     fps_id: int = 6,
-    version: str = "svd_xt_1-1",
+    version: str = "svd_xt",
     cond_aug: float = 0.02,
     decoding_t: int = 3,  # Number of frames decoded at a time! This eats most VRAM. Reduce if necessary.
     device: str = "cpu",
